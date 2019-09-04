bbf5998645bfcaaba9168ce796d02a3ed80f0b57
VietLH <vietlh@VietLHs-MacBook-Pro.local>
2019-09-04 18:10:49 +0700

    update database in index

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
