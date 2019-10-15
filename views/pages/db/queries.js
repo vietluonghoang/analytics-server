527d68cf622e141986bd54b3bde7d20d889c2c15
VietLH <vietlh@VietLHs-MacBook-Pro.local>
2019-10-15 17:56:08 +0700

    test coupon

--- views/pages/db/queries.js
+++ views/pages/db/queries.js
@@ -230,6 +230,9 @@ const redeemAdsOptoutCoupon = (request, response) => {
         throw error
       }
       //if result is 1 then there is still available coupon (0 is not)
+      console.log('check available coupon result: ', results);
+      console.log('check available coupon result isValid: ', results.isValid);
+      console.log('check available coupon result rowCount: ', results.rowCount);
       if (results.isValid == 1) {
         //check if the user has already redeemed this coupon code
         pool.query('select ads_id from ads_optout where ads_id = $1 and device_name = $2 and coupon_code = $3', [adsidLowercase, devicenameLowercase, couponCodeLowercase], (error, results) => {
@@ -237,7 +240,9 @@ const redeemAdsOptoutCoupon = (request, response) => {
             throw error
           }
           //if the user has already redeemed the coupon, just update the expired_time as set in coupons table accordingly
+          console.log('check available redeemed coupon: ', results.rowCount);
           if(results.rowCount >= 1){
+            console.log('updating expired time of existing coupon');
             pool.query('update ads_optout set expired_time = (select valid_until from coupons where coupon_code = $3) where ads_id = $1 and device_name = $2 and coupon_code = $3', [adsidLowercase, devicenameLowercase, couponCodeLowercase], (error, results) => {
               if (error) {
                 throw error
@@ -246,6 +251,7 @@ const redeemAdsOptoutCoupon = (request, response) => {
             })
           }else{
             //if the user has not redeemed the coupon, add a record for the coupon
+            console.log('insert coupon for user');
             pool.query('insert into ads_optout(ads_id,device_name,last_redeemed_code,expired_time,redeemed_time) values ($1,$2,$3,(select valid_until from coupons where coupon_code = $3),$4)', [adsidLowercase, devicenameLowercase, couponCodeLowercase,timestamp], (error, results) => {
               if (error) {
                 throw error
